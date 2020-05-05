40d7d04fb658a48f67c3e137828b479ec3e18f58
imashi0804 <sandalirajapaksha10@gmail.com>
2020-05-05 22:11:22 +0530

    create html form and java script file

--- Hospital/WebContent/component/hostpital.js
+++ Hospital/WebContent/component/hostpital.js
@@ -1,12 +1,174 @@
-$(document).ready(function() {
+$(document).on("click", ".btnUpdate", function(event) { 
 	
-	if ($("#alertSuccess").text().trim() == "") {
-	    $("#alertSuccess").hide();		
-	}
-	$("#alertError").hide();
+	$("#hidItemIDSave").val($(this).closest("tr").find('#hidItemIdUpdate').val());
+	$("#hostpitalName").val($(this).closest("tr").find('#hidhidUpdate').val());
+	$("#docterName").val($(this).closest("tr").find('#hiddidUpdate').val());
+	$("#description").val($(this).closest("tr").find('td:eq(5)').text());
+	$("#date").val($(this).closest("tr").find('td:eq(1)').text());
+
 	
-	
+	$("#alertSuccess").text().trim() == "Data Retrived"
+
 });
 
 
 
+$(document).ready(function () {
+	 document.forms['form'].reset();
+	 
+});
+
+$(document).ready(function() { 
+
+	if($("#alertSuccess").text().trim() == ""){
+		$("#alertSuccess").hide();
+	}
+	
+	$("#alertError").hide(); 
+	
+});
+
+$(document).on("click", "#btnSave", function(event) { 
+	
+	$("#alertSuccess").text("");  
+	$("#alertSuccess").hide();  
+	$("#alertError").text(""); 
+	$("#alertError").hide(); 
+	
+	var status = validateItemForm(); 
+	
+	if (status != true)  {  
+		$("#alertError").text(status);  
+		$("#alertError").show();  
+		return; 	
+		} 
+	
+	var type = ($("#hidItemIDSave").val() == "") ? "POST" : "PUT"; 
+	
+	$.ajax( { 
+		url : "hostpitalAPI", 
+		type : type,  
+		data : $("#detailform").serialize(),  
+		dataType : "text",  
+		complete : function(response, status) 
+		{   
+			onItemSaveComplete(response.responseText, status);  
+		
+		} 
+	}); 
+	
+});
+
+function onItemSaveComplete(response, status) {  
+	console.log(response);
+	var resultSet = JSON.parse(response); 
+	 
+	if (resultSet.status.trim() == "success") {  
+		
+		$("#alertSuccess").text("Successfully saved.");  $("#alertSuccess").show(); 
+		 
+		 $("#divItemsGrid").html(resultSet.data); 
+	
+	} else if (resultSet.status.trim() == "error") 
+	
+	{  
+		$("#alertError").text(resultSet.data); 
+		$("#alertError").show(); 
+		
+	}
+	else if (status == "error") {
+		
+		$("#alertError").text("Error while saving.");  
+		$("#alertError").show(); 
+	}
+	 else 
+	 {  
+		 $("#alertError").text("Unknown error while saving.."); 
+		 $("#alertError").show(); 
+	 }
+	
+	$("#hidItemIDSave").val(""); 
+	$("#form")[0].reset(); 
+}
+
+
+$(document).on("click", ".btnRemove", function(event) { 
+	
+	console.log(event);
+	$.ajax( { 
+		url : "hostpitalAPI",   
+		type : "DELETE",   
+		data : "AppointmentID =" + $(this).data("itemid"),   
+		dataType : "text",   
+		complete : function(response, status) 
+		{   
+			onItemDeleteComplete(response.responseText, status);  
+		
+		} 
+	}); 
+	
+});
+
+function onItemDeleteComplete(response, status) {  
+	
+	var resultSet = JSON.parse(response); 
+	 
+	if (resultSet.status.trim() == "success") {  
+		
+		$("#alertSuccess").text("Successfully deleted."); 
+		$("#alertSuccess").show(); 
+		 
+		 $("#divItemsGrid").html(resultSet.data); 
+	
+	} else if (resultSet.status.trim() == "error") 
+	
+	{  
+		$("#alertError").text(resultSet.data); 
+		$("#alertError").show(); 
+		
+	}
+	else if (status == "error") {
+		
+		$("#alertError").text("Error while deletingdeleting.");  
+		$("#alertError").show(); 
+	}
+	 else 
+	 {  
+		 $("#alertError").text("Unknown error while deleting.."); 
+		 $("#alertError").show(); 
+	 }
+	
+}
+
+
+
+	
+	
+
+function validateItemForm() {  
+	
+	if ($("#docterName").val().trim() == 0) {  
+		
+		return "Please Select a docter";
+	} 
+	
+	if ($("#hostpitalName").val().trim() == 0) {  
+		 
+		return "Please Select a hostpital";
+	} 
+	
+	if ($("#description").val().trim() == "") {  
+
+	return "Please Enter description";
+	} 
+	
+	if ($("#date").val().trim() == "dd/mm/yyyy") {  
+
+		return "Please Select Preferd Date";
+	} 
+
+	
+	
+	return true; 
+	 
+	}
--- Hospital/WebContent/hospital.jsp
+++ Hospital/WebContent/hospital.jsp
@@ -1,6 +1,7 @@
-	<%@page import="model.hospital"%>
+
+<%@page import="model.hospital"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,15 +13,72 @@
 </head>
 <body>
 
+	<div class="container">
+
+		<div id="signupbox" margin-top:50px" class="mainbox">
+			<div class="panel panel-info">
+				<div class="panel-heading">
+					<div class="panel-title">Hospital management</div>
+				</div>
+				<div class="panel-body">
+					<form id="signupform" class="form-horizontal" role="form">
+
+						<div id="alertSuccess" class="alert alert-success"></div>
+
+						<div id="alertError" class="alert alert-danger"></div>
+ <!-- `hospitalName`, `hospitalAddress`, `phone`, `hospitalEmail`, `password` -->
+						<div class="form-group">
+							<label for="firstname" class="col-md-3 control-label"> hospital Name </label>
+							<div class="col-md-9">
+								<input type="text" class="form-control" name="hospitalName"
+									placeholder="Last Name" id="hospitalName">
+							</div>
+						</div>
 
 
-<div id="divItemsGrid" style="margin-top:10px;">
-		<% 
-		
-		hospital Aobj2 = new  hospital();
+						<div class="form-group">
+							<label for="firstname" class="col-md-3 control-label">hospital Address</label>
+							<div class="col-md-9">
+								<input type="text" class="form-control" name="hospitalAddress"
+									placeholder="Last Name" id="hospitalAddress">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="email" class="col-md-3 control-label">phone</label>
+							<div class="col-md-9">
+								<input type="text" class="form-control" name="phone"
+									placeholder="phone number" id="phone">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="lastname" class="col-md-3 control-label">Description</label>
+							<div class="col-md-9">
+								<input type="text" class="form-control" name="lastname"
+									placeholder="Last Name" id="description">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="password" class="col-md-3 control-label">Appointment
+								date</label>
+							<div class="col-md-9">
+								<input type="password" class="form-control" name="password"
+									placeholder="password" id="password">
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div id="divItemsGrid" style="margin-top: 10px;">
+		<%
+			hospital Aobj2 = new hospital();
 		out.print(Aobj2.getHospitalDetails());
-		
-		%> 
-</div> 
+		%>
+	</div>
 </body>
 </html>
